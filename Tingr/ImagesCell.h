3638392c066c39e77203012d0e7911126832232e
imaginationcoder <labs@maisasolutions.com>
2017-10-18 13:53:30 +0530

    Updates for home page

---
+++ Tingr/ImagesCell.h
@@ -0,0 +1,13 @@
+//
+//  ImagesCell.h
+//  Tingr
+//
+//  Created by Maisa Pride on 9/10/17.
+//  Copyright © 2017 Maisa Pride. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesCell : UITableViewCell
+
+@end
---
+++ Tingr/ImagesCell.m
@@ -0,0 +1,24 @@
+//
+//  ImagesCell.m
+//  Tingr
+//
+//  Created by Maisa Pride on 9/10/17.
+//  Copyright © 2017 Maisa Pride. All rights reserved.
+//
+
+#import "ImagesCell.h"
+
+@implementation ImagesCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
